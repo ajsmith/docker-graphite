8ae4317f53f332dd41faa5c3c78601f98ad1fb18
Alex Smith <alex.smith@redhat.com>
2015-08-07 17:33:41 -0400

    Remove commented out lines.

--- mariadb/graphite.sql
+++ mariadb/graphite.sql
@@ -1,7 +1,3 @@
 create user 'graphite';
 create database graphite;
 grant all on graphite.* to 'graphite';
-
--- create user 'graphite'@'%';
--- create database graphite;
--- grant all on graphite.* to 'graphite'@'%';
