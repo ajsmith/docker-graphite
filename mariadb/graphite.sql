92f6b439e328e167ce6a46e2dc3883eb3d4a3393
Alex Smith <alex.smith@redhat.com>
2015-08-07 17:31:28 -0400

    Add MariaDB image resources.

---
+++ mariadb/graphite.sql
@@ -0,0 +1,7 @@
+create user 'graphite';
+create database graphite;
+grant all on graphite.* to 'graphite';
+
+-- create user 'graphite'@'%';
+-- create database graphite;
+-- grant all on graphite.* to 'graphite'@'%';
